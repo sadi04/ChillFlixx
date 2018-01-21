4ba37e307ae9e5fff837bea377093c77b7959b5b
sadi04 <1405016.sas@ugrad.cse.buet.ac.bd>
2018-01-21 11:00:47 +0600

    commit
    Nav_drawer baki

--- app/src/main/java/com/esoxjem/movieguide/listing/MoviesListingActivity.java
+++ app/src/main/java/com/esoxjem/movieguide/listing/MoviesListingActivity.java
@@ -2,9 +2,15 @@ package com.esoxjem.movieguide.listing;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
+import android.view.MenuItem;
 
 import com.esoxjem.movieguide.R;
 import com.esoxjem.movieguide.Constants;
@@ -12,7 +18,7 @@ import com.esoxjem.movieguide.details.MovieDetailsActivity;
 import com.esoxjem.movieguide.details.MovieDetailsFragment;
 import com.esoxjem.movieguide.Movie;
 
-public class MoviesListingActivity extends AppCompatActivity implements MoviesListingFragment.Callback
+public class MoviesListingActivity extends AppCompatActivity implements MoviesListingFragment.Callback,NavigationView.OnNavigationItemSelectedListener
 {
     public static final String DETAILS_FRAGMENT = "DetailsFragment";
     private boolean twoPaneMode;
@@ -22,7 +28,23 @@ public class MoviesListingActivity extends AppCompatActivity implements MoviesLi
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        setToolbar();
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        if (getSupportActionBar() != null)
+        {
+            getSupportActionBar().setTitle(R.string.movie_guide);
+            getSupportActionBar().setDisplayShowTitleEnabled(true);
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
 
         if (findViewById(R.id.movie_details_container) != null)
         {
@@ -40,15 +62,13 @@ public class MoviesListingActivity extends AppCompatActivity implements MoviesLi
         }
     }
 
-    private void setToolbar()
-    {
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        if (getSupportActionBar() != null)
-        {
-            getSupportActionBar().setTitle(R.string.movie_guide);
-            getSupportActionBar().setDisplayShowTitleEnabled(true);
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
         }
     }
 
@@ -98,5 +118,30 @@ public class MoviesListingActivity extends AppCompatActivity implements MoviesLi
         getSupportFragmentManager().beginTransaction()
                 .replace(R.id.movie_details_container, movieDetailsFragment, DETAILS_FRAGMENT)
                 .commit();
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.nav_profile) {
+            // Handle the camera action
+            }
+        else if (id == R.id.nav_watched) {
+
+
+        } else if (id == R.id.nav_friends) {
+
+        } else if (id == R.id.nav_settings) {
+
+        } else if (id == R.id.nav_explore) {
+
+        } else if (id == R.id.nav_logOut) {
+
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
     }
 }
